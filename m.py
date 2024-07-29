e3e42f490eff4cf83aa354b5af62e587f97468a9
aniruddha090 <78275932+aniruddha090@users.noreply.github.com>
2024-07-29 19:50:47 +0530

    Update m.py

--- m.py
+++ m.py
@@ -10,9 +10,9 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEM')
+bot = telebot.TeleBot('7399735507:AAHrbAgM3NLApic89e57d1C87U-0876Sg-8')
 # Admin user IDs
-admin_id = {"5588464519"}
+admin_id = {"6092284993"}
 
 # Files for data storage
 USER_FILE = "users.json"
